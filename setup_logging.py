4b45520c1568ae5cd3a2e39cad73ca568906ad4a
RichardProsser <ebizbytes@gmail.com>
2015-03-16 17:32:24 +0000

    Attempt to use "user-defined object" with '()' key but gets error: ValueError: Unable to configure handler 'debug_file_handler': ... at line 26 of 'setup_logging.py'. Cause unknown.

--- setup_logging.py
+++ setup_logging.py
@@ -1,23 +1,28 @@
 import os
 import logging.config
-
 import yaml
+import pdb
 
-def setup_logging(
-    default_path='application/logging.yaml',
-    default_level=logging.INFO,
-    env_key='LOG_CFG'
-):
-    """Setup logging configuration
+def get_log_path(name=None):
+    log_path = os.getenv('LOG_PATH', 'logs')
+    # return log_path if name is None else os.path.join(log_path, name)
+    return log_path if name is None else log_path + '/' + name
 
-    """
-    path = default_path
-    value = os.getenv(env_key, None)
-    if value:
-        path = value
+def setup_logging(default_path='logging.yaml', default_level=logging.INFO, env_key='LOG_CFG'):
+    """Setup logging configuration. """
+
+    pdb.set_trace()
+    path = os.getenv(env_key, default_path)
     if os.path.exists(path):
         with open(path, 'rt') as f:
             config = yaml.load(f.read())
+
+        # Make sure that directory for logs exists.
+        try:
+            os.mkdir(get_log_path())
+        except OSError as e:
+            pass
+
         logging.config.dictConfig(config)
     else:
         logging.basicConfig(level=default_level)
